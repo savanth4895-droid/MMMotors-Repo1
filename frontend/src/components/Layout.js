b647f04ae1cd0f35cf8f7cbe918b790b5939dabd
mmmptorsonline3123 <mmmptorsonline3123@gmail.com>
2026-03-19 07:40:53 +0530

    Refactor import paths and move MotorcycleIcon component

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { AuthContext } from '../App';
+import { AuthContext } from '../contexts/AuthContext';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Button } from './ui/button';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
@@ -23,15 +23,8 @@ import {
 } from 'lucide-react';
 
 // Custom Motorcycle Icon Component
-const MotorcycleIcon = ({ className = "w-5 h-5" }) => (
-  <svg className={className} fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 16c0 1.1.9 2 2 2s2-.9 2-2-.9-2-2-2-2 .9-2 2zm12 0c0 1.1.9 2 2 2s2-.9 2-2-.9-2-2-2-2 .9-2 2zm1.5-9H16l-3-3h-2v2h1.5l1.5 1.5H8L6 5.5H4.5C3.7 5.5 3 6.2 3 7s.7 1.5 1.5 1.5H6l2 2.5h8l2-2.5h.5c.8 0 1.5-.7 1.5-1.5s-.7-1.5-1.5-1.5z"/>
-    <circle cx="7" cy="16" r="1"/>
-    <circle cx="19" cy="16" r="1"/>
-    <path d="M8.5 12l1.5-1.5h4L15.5 12H8.5z"/>
-  </svg>
-);
 import { toast } from 'sonner';
+import MotorcycleIcon from './ui/MotorcycleIcon';
 
 const Layout = ({ children }) => {
   const { user, logout } = useContext(AuthContext);
